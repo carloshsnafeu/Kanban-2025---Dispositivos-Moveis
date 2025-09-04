2c3f49f2c017179247617b5c0318926f09d86b9a
20231tiimi0014 <carloshsnafeu@gmail.com>
2025-09-04 08:52:06 -0300

    Passo 82 (feito)

--- app/src/main/java/com/carlos/task/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/carlos/task/ui/auth/LoginFragment.kt
@@ -28,6 +28,10 @@ class LoginFragment : Fragment() {
     }
 
     private fun initListener() {
+        binding.btnLogin.setOnClickListener {
+            findNavController().navigate(R.id.action_global_homeFragment)
+        }
+
         binding.tvCreateAccount.setOnClickListener {
             findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
         }
